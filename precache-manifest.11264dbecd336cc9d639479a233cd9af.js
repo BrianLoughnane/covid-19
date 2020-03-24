e51cc3ba6b524143710c3ee5928e4ab781d9f1bf
BrianLoughnane <BrianJLoughnane@gmail.com>
2020-03-24 09:37:59 -0700

    add build folder

---
+++ precache-manifest.11264dbecd336cc9d639479a233cd9af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26d558b4d393e89a85d45dd5cac6a27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49561b78f54cd3a5a039",
+    "url": "/static/css/main.2f0cbbb2.chunk.css"
+  },
+  {
+    "revision": "839dd817bedae911c0b5",
+    "url": "/static/js/2.115df04d.chunk.js"
+  },
+  {
+    "revision": "ed8b678df6934e809073f59befab3889",
+    "url": "/static/js/2.115df04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49561b78f54cd3a5a039",
+    "url": "/static/js/main.fc4e9ba8.chunk.js"
+  },
+  {
+    "revision": "8d8d139b190630b6bc42",
+    "url": "/static/js/runtime-main.7ba82ef7.js"
+  }
+]);

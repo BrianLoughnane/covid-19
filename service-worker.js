e9f9d3efd9cf690a60097cf4fd88ab401f139a92
BrianLoughnane <BrianJLoughnane@gmail.com>
2020-04-26 16:30:41 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e5fba317b583c770fb7563c6f4d98c29.js"
+  "/precache-manifest.ede985f7f752c2b44370bac770c0431d.js"
 );
 
 self.addEventListener('message', (event) => {
